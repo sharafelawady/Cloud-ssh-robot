a8d711c408a855b37b9b9c39f74d01fdb6e910b3
Sharaf Elawady <64039479+sharafelawady@users.noreply.github.com>
2020-05-27 22:07:56 +0200

    Add files via upload

---
+++ var speed drive/script/robot getkey with arduino speed.py
@@ -0,0 +1,48 @@
+import serial
+from getkey import getkey , keys
+ser = serial.Serial('/dev/ttyACM1', baudrate = 9600, timeout = 1)
+
+def getvalues ():
+    ser.write(b'g')
+    arduinodata = ser.readline()
+    return arduinodata[0]
+
+while (1):
+    key = getkey()
+
+    if key == '2':
+        ser.write(b"1")
+        print('robot is moving backward')
+    elif key == '8':
+        ser.write(b"2")
+        print('robot is moving forward')
+    elif key == '6':
+        ser.write(b"5")
+        print('robot turn right')
+    elif key == '4':
+        ser.write(b"4")
+        print('robot turn left')
+    elif key == '5':
+        ser.write(b"3")
+        print('stop')    
+    #elif key == '1':
+    #    ser.write(b"6")
+    #    print("robot is moving backward 45 left")
+    #elif key == '3':
+    #    ser.write(b"7")
+    #    print("robot is moving backward 45 right")###
+    elif key == '7':
+        ser.write(b"8")
+        print("robot is moving forward 45 left")
+    elif key == '9':
+        ser.write(b"9")
+        print("robot is moving forward 45 right")
+    elif key == '3':
+        ser.write(b"7")
+        print("increasing speed by ten")
+    elif key == '1':
+        ser.write(b"6")
+        print("decreasing speed by ten")
+    else:
+        print("unkown key") 
+
